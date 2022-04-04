4af1f668d2df4413f516605c462481bf88f39287
Cesar San <cesanchezesc@gmail.com>
2022-04-04 10:03:00 -0500

    app working

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,25 +1,54 @@
-// TODO: Install the following package:
+// contactDB: Install the following package:
 import { openDB } from 'idb';
 
-// TODO: Complete the initDb() function below:
+// contactDB: Complete the initDb() function below:
 const initdb = async () => {
-
+    openDB('contactsDB', 1, {
+        upgrade(db) {
+          if (db.objectStoreNames.contains('contactsDB')) {
+            console.log('contactsDB database already exists');
+            return;
+          }
+          db.createObjectStore('contactDBs', { keyPath: 'id', autoIncrement: true });
+          console.log('contactDBs database created');
+        },
+      });
 };
 
 
-// TODO: Complete the postDb() function below:
+// contactDB: Complete the postDb() function below:
 export const postDb = async (name, home, cell, email)  => {
- 
+    console.log('Post to the ase');
+    const contactDBsDb = await openDB('contactDBs', 1);
+    const tx = contactDBsDb.transaction('contactDBs', 'readwrite');
+    const store = tx.objectStore('contactDBs');
+    const request = store.add({ contactDB: content });
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
 };
 
-// TODO: Complete the getDb() function below:
+// contactDB: Complete the getDb() function below:
 export const getDb = async () => {
-  
+    console.log('GET all from the database');
+    const contactDBsDb = await openDB('contactDBs', 1);
+    const tx = contactDBsDb.transaction('contactDBs', 'readonly');
+    const store = tx.objectStore('contactDBs');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result;
 };
 
-// TODO: Complete the deleteDb() function below:
+// contactDB: Complete the deleteDb() function below:
 export const deleteDb = async (id) => {
-  
+    console.log('DELETE from the database', id);
+    const contactDBsDb = await openDB('contactDBs', 1);
+    const tx = contactDBsDb.transaction('contactDBs', 'readwrite');
+    const store = tx.objectStore('contactDBs');
+    const request = store.delete(id);
+    const result = await request;
+    console.log('result.value', result);
+    return result;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const {InjectManifest} = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+
 
 module.exports = () => {
   return {
@@ -8,20 +12,68 @@ module.exports = () => {
       install: './src/js/install.js',
       cards: './src/js/cards.js'
     },
+    // devServer: {
+    //   hot: 'only',
+    // },
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+        new HtmlWebpackPlugin({
+          template: './index.html',
+          title: 'Contact-Directory'
+        }),
+        new InjectManifest({
+          swSrc: './src-sw.js',
+          swDest: 'src-sw.js',
+        }),
+        new WebpackPwaManifest({
+          fingerprints: false,
+          inject: true,
+          name: 'Contact Cards',
+          short_name: 'Contact',
+          description: 'Never forget your contacts!',
+          background_color: '#225ca3',
+          theme_color: '#225ca3',
+          start_url: '/',
+          publicPath: '/',
+          icons: [
+            {
+              src: path.resolve('src/images/logo.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons'),
+            },
+          ],
+        }), 
     ],
+   
 
     // TODO: Add the correct modules
     module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
 
     }
-  };
+  }
 };
+
